93ee3f1ef316d8b839f8570739699c0576713783
mahimaupadhya <62554816+mahimaupadhya@users.noreply.github.com>
2020-09-21 13:54:52 +0530

    Update WhatsYourName.py

--- Python/WhatsYourName.py
+++ Python/WhatsYourName.py
@@ -1,7 +1,7 @@
-def split_and_join(line):
-    return line.replace(" ","-")
+def print_full_name(a, b):
+    print("Hello "+a+ " "+b+"! You just delved into python.")
 
 if __name__ == '__main__':
-    line = input()
-    result = split_and_join(line)
-    print(result)
+    first_name = input()
+    last_name = input()
+    print_full_name(first_name, last_name)
