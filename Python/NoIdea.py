e9de35d4e9ff37a03f66a82d253e146a684e84d2
mahimaupadhya <62554816+mahimaupadhya@users.noreply.github.com>
2020-09-21 14:08:04 +0530

    Create NoIdea.py

---
+++ Python/NoIdea.py
@@ -0,0 +1,10 @@
+,m=map(int,input().split())
+arr=list(map(int,input().split()))
+a=set(map(int,input().split()))
+b=set(map(int,input().split()))
+count=0
+for i in arr:
+    if i in a: count+=1
+    elif i in b: count-=1
+    else:continue
+print(count)
